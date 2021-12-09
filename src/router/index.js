87afe5c5dd9e0e67c972ca7cfc5c6919cdb025b2
Federico <federicoaacevedo@hotmail.com>
2021-12-09 20:11:20 -0300

    Login&Logout

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import store from '../store'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta:{rutaProtegida: true}
   },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
+
   {
     path: '/editar/:id',
     name: 'Editar',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Editar.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Editar.vue'),
+    meta:{rutaProtegida: true}
   },
   {
     path: '/registro',
@@ -32,6 +31,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) =>{
+  if(to.meta.rutaProtegida){
+    if(store.getters.usuarioAutenticado){
+      next()
+    }else{
+      next('/ingreso')
+    }
+  }else{
+    next();
+  }
 })
 
 export default router
